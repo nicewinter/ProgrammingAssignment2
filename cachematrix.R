e27ca87f6aaa5490b6a593c0e69e9294b7905101
nicewinter <nicewinter68@hotmail.com>
2015-07-03 17:44:13 +0100

    change
    change

--- cachematrix.R
+++ cachematrix.R
@@ -4,7 +4,15 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-fadadf
+    reverse<-NULL
+    set <- function(y){
+        x<<-y
+        reverse<<-NULL
+    }
+    get <- function() x
+    setReverse <- function(r) reverse<<-r
+    getReverse <- function() reverse
+    list(set=set, get=get, setReverse=setReverse, getReverse=getReverse)
 }
 
 
@@ -12,4 +20,13 @@ fadadf
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+        reverse<-x$getReverse()
+        if (!is.null(reverse)){
+            message("getting cached reverse")
+            return(reverse)
+        }
+        matrix<-x$get()
+        reverse<-solve(matrix)
+        x$setReverse(reverse)
+        reverse
 }
